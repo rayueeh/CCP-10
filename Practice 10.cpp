c2d40dbb8abe554f979d46762cc14f33bd29c267
Brigette Nalangan <146305223+rayueeh@users.noreply.github.com>
2023-10-03 10:16:39 +0800

    Add files via upload

---
+++ Practice 10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	
+	int age;
+	
+	cout<<"Enter your age: ";
+	cin>>age;
+	
+	if(age >= 18){
+	  cout<<"You are in a legal age\n";
+	  cout<<"Pwede ka eon makulong!";
+	  
+	
+}else{
+	cout<<"You are a minor\n";
+	cout<<"Bata ka pa";
+	
+	return 0;
+}
